a4e3a65444cfb220301bac0506280add8d0dc3eb
Valeria <2601@bilinguesanjuan.edu.gt>
2025-09-26 19:42:39 -0600

    Clientescontrol

--- app/Controllers/clientesController.php
+++ app/Controllers/clientesController.php
@@ -1,53 +1,68 @@
 <?php
-    namespace App\Controllers;
- 
+namespace App\Controllers;
+
 use App\Models\Clientes;
 use CodeIgniter\Controller;
- 
-    class ControladorClientes extends BaseController{
-        public function verClientes(){
-            $cliente = new Clientes();
-            $datosBD['datosBD'] = $cliente->findAll();
-            return view('Registro de usuarios',$datosBD);
-        }
- 
-        public function guardarClientes(){
-            $codigo = $this->request->getVar('txt_codigo');
-            $nombre = $this->request->getVar('txt_nombre');
-            $cod_depto = $this->request->getVar('txt_cod_depto');
- 
-            $muni = new Municipio();
-            $datos=['cod_muni'=>$codigo,
-                    'nombre_municipio'=>$nombre,
-                    'cod_depto'=>$cod_depto];
-            $muni->insert($datos);
-            return $this->verMunicipios();
-        }
- 
-        public function eliminarMunicipio($codigo=null){
-            $municipio = new Municipio();
-            $municipio->delete($codigo);
-            return $this->verMunicipios();
-        }
- 
-        public function localizarMunicipio($codigo=null){  
-            $municipio = new Municipio();
-            $datosMunicipio['datosMunicipio']=$municipio->where('cod_muni',$codigo)->first();
-            return view('frm_actualizarMunicipio',$datosMunicipio);  
-        }
- 
-        public function modificarMunicipio(){
-            $codigo = $this->request->getVar('txt_codigo');
-            $nombre = $this->request->getVar('txt_nombre');
-            $codigoDepto = $this->request->getVar('txt_cod_depto');
- 
-            $municipio = new Municipio();
-        $datos=[
-            'nombre_municipio'=>$nombre,
-            'cod_depto'=>$codigoDepto];
- 
- 
-            $municipio->update($codigo,$datos);
-            return $this->verMunicipios();
-        }
+
+class ControladorClientes extends BaseController {
+
+    // Mostrar todos los clientes
+    public function verClientes() {
+        $cliente = new Clientes();
+        $datosBD['datosBD'] = $cliente->findAll();
+        return view('Registro_de_usuarios', $datosBD);
     }
+
+    // Guardar nuevo cliente
+    public function guardarClientes() {
+        $id = $this->request->getVar('txt_id');
+        $nombre = $this->request->getVar('txt_nombre');
+        $apellido = $this->request->getVar('txt_apellido');
+        $correo = $this->request->getVar('txt_correo');
+        $telefono = $this->request->getVar('txt_telefono');
+
+        $cliente = new Clientes();
+        $datos = [
+            'id_cliente' => $id,
+            'nombre' => $nombre,
+            'apellido' => $apellido,
+            'correo' => $correo,
+            'telefono' => $telefono
+        ];
+        $cliente->insert($datos);
+        return $this->verClientes();
+    }
+
+    // Eliminar cliente
+    public function eliminarCliente($id = null) {
+        $cliente = new Clientes();
+        $cliente->delete($id);
+        return $this->verClientes();
+    }
+
+    // Localizar cliente para editar
+    public function localizarCliente($id = null) {
+        $cliente = new Clientes();
+        $datosCliente['datosCliente'] = $cliente->where('id_cliente', $id)->first();
+        return view('frm_actualizarCliente', $datosCliente);
+    }
+
+    // Modificar cliente
+    public function modificarCliente() {
+        $id = $this->request->getVar('txt_id');
+        $nombre = $this->request->getVar('txt_nombre');
+        $apellido = $this->request->getVar('txt_apellido');
+        $correo = $this->request->getVar('txt_correo');
+        $telefono = $this->request->getVar('txt_telefono');
+
+        $cliente = new Clientes();
+        $datos = [
+            'nombre' => $nombre,
+            'apellido' => $apellido,
+            'correo' => $correo,
+            'telefono' => $telefono
+        ];
+        $cliente->update($id, $datos);
+        return $this->verClientes();
+    }
+}
